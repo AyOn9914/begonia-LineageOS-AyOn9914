bd51e589f782463c1e03863e0a3040105d072164
AyOn9914 <141134915+AyOn9914@users.noreply.github.com>
2023-08-01 20:15:43 +0600

    Add files via upload

---
+++ build_rom.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# sync rom
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b 11.x
+git clone https://github.com/AyOn9914/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+
+# build rom
+source build/envsetup.sh
+lunch lineage_begonia-userdebug
+make -j$(nproc --all)
+
+# upload rom
+up(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
+}
+
+up out/target/product/mido/*.zip
